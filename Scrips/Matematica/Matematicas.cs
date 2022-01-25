632f23ac877433d909df18053dfa5240555ecd22
Mondongo <98285409+Mondongou@users.noreply.github.com>
2022-01-25 17:28:18 -0400

    Planeta y matematica

---
+++ Scrips/Matematica/Matematicas.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // suelta un numoro aleatorio
+     public static int numAl(int min, int max)
+    {
+        int value = 0;
+        for (int i = 0; i <= 10; i++)
+        {
+            var guid = System.Guid.NewGuid();
+            var justNumbers = new System.String(guid.ToString().Where(System.Char.IsDigit).ToArray());
+            var seed = int.Parse(justNumbers.Substring(0,9));
+            var random = new System.Random(seed);
+            value = random.Next(min, max);
+        }
+        return value;
+    }
+    // limpia los arrays dejando solos numeros
+    public static int limpiezaString( GameObject gm, int sub , int rem = 1)
+    {
+        string lim = gm.name.Substring(sub);
+        if (rem > 0)
+        {
+            lim = lim.Remove(rem);
+        }
+        return int.Parse(lim);
+    }
+    public static int ubicacion(int pos , int can)
+    {
+        int[] n = new int[can];
+        n[0] = can;
+        for (int i = 1; i < n.Length; i++)
+        {
+            int y = i;
+            y--;
+            n[i] = n[y] - 1;
+        }
+        pos--;
+        return n[pos];
+    }
+}
---
+++ Scrips/planeta/Planeta.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Planeta : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        
+
+    }
+
+
+    void Update()
+    {
+
+
+
+
+    }
+
+}
+
